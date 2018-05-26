b14c0958628c6c24b08007312bdd28ba56971f13
Jonathan Chan <kcjonathanchan@outlook.com>
2018-05-26 03:41:26 -0700

    Added CORs restricted settings

--- main.go
+++ main.go
@@ -18,6 +18,13 @@ func main() {
 	e.Logger.SetOutput(os.Stdout)
 	e.Logger.SetLevel(log.DEBUG)
 
+	// CORS restricted
+	// Allows requests from "http://localhost:3000" origin with POST method
+	e.Use(middleware.CORSWithConfig(middleware.CORSConfig{
+		AllowOrigins: []string{"http://localhost:3000"},
+		AllowMethods: []string{echo.POST},
+	}))
+
 	_ = server.New(e)
 
 	// Start server
